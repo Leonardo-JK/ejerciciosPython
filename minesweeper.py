b1f05bd0041101df9224280a45318f10aa84cf30
Leonardo-JK <leok_86@hotmail.com>
2022-07-09 23:57:55 -0300

    Correction on minesweeper

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,7 @@
 import random
-from time import time
-from numpy import prod, product
+import time
+from typing import final
+from numpy import prod
 
 start_time = 0
 final_time = 0
@@ -8,15 +9,16 @@ class Board:
     def __init__(self, size_width, size_heigth, bombs_num):
         self.bombs_num = bombs_num
         self.size = [size_heigth, size_width]
-        self.board_values = [[None for i in range(size_width)] for j in range(size_heigth)]
-        self.board_show = [[False for i in range(size_width)] for j in range(size_heigth)]
-        self.free_position = (size_heigth * size_width) - bombs_num
+        self.board_values = [[None for i in range(size_width)] for j in range(size_heigth)] #This matrix will be contain bombs' index and number of bombs neighbors
+        self.board_show = [[False for i in range(size_width)] for j in range(size_heigth)]  #This matrix contain the position which are visible.
+        self.free_position = (size_heigth * size_width) - bombs_num 
     
     
     def free(self):
         print(self.free_position)
         
-    
+        
+    #Plant bombs randomly
     def put_bombs(self):
         bombs = 0
         
@@ -43,6 +45,7 @@ class Board:
             print("|")                                                                      #
     #########################################################################################
     
+    #Display the board on screen
     def show_board(self):
         for i in range(self.size[1] + 1):
             print(('%6s' % ' ') if i == 0 else '%2s' % str(i - 1), end=" ")
@@ -62,15 +65,18 @@ class Board:
             print("")
     
     
+    #Reset the default values to start a new game
     def reset(self):
         self.board_values = [[None for i in range(self.size[1])] for j in range(self.size[0])]
         self.board_show = [[False for i in range(self.size[1])] for j in range(self.size[0])]
-        self.free_position = prod(self.size)
+        self.free_position = prod(self.size) - self.bombs_num
+
             
-            
+    #Create a new board      
     def new_board(self):
         self.put_bombs()
         
+        #Count the number of bombs neighbors for each position and set this number on the matrix
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 
@@ -85,62 +91,64 @@ class Board:
                                 total_bombs += 1   
                                                 
                     self.board_values[i][j] = total_bombs
-            
-        self.solution()
-        print("")
         
-        
+    
+    #It take the choised position by coordinate and determinate how to proceed
     def look_at(self, row, col):
-        
-        if row == None:
-            return True
-        
-        if self.board_values[row][col] == "*": 
-            for i in range(self.size[0]):
-                for j in range(self.size[1]):
-                    if self.board_values[i][j] == "*":
-                        self.board_show[i][j] = True
-            self.show_board()
-            print("You loose!")
-            return False
-            
-        if self.board_values[row][col] > 0:
-            self.board_show[row][col] = True
-            self.free_position -= 1
-            
-            self.show_board()
-        
-        
-        def clear_location(r, c):
-            if self.board_values[r][c] == 0 and self.board_show[r][c] == True:
-                for i in range(max(0, r - 1), min(self.size[0], r + 2)):
-                    for j in range(max(0, c - 1), min(self.size[1], c + 2)):
-                        if self.board_values[i][j] != "*" and self.board_values[i][j] != 0 and self.board_show[i][j] == False:
+        try:
+            #If you dig a mine, then show all mines on board and finish the game
+            if self.board_values[row][col] == "*": 
+                for i in range(self.size[0]):
+                    for j in range(self.size[1]):
+                        if self.board_values[i][j] == "*":
                             self.board_show[i][j] = True
-                            self.free_position -= 1
-                        if self.board_values[i][j] == 0 and self.board_show[i][j] == False:
-                            self.board_show[i][j] = True
-                            self.free_position -= 1
-                            clear_location(i,j)
-                                    
+                self.show_board()
+                global final_time
+                final_time = time.time()                
+                print("You loose!")
+                print("Your time: ", final_time - start_time, "seconds.")
+                return False
+                
+            if self.board_values[row][col] > 0:
+                self.board_show[row][col] = True
+                self.free_position -= 1
+                self.show_board()
             
-        if self.board_values[row][col] == 0:
-            self.board_show[row][col] = True
-            self.free_position -= 1
-            clear_location(row, col)
-            self.show_board()    
-        
-        if self.free_position > 0:    
+            #This function will clear all the region which no contain bombs.
+            def clear_location(r, c):
+                if self.board_values[r][c] == 0 and self.board_show[r][c] == True:
+                    for i in range(max(0, r - 1), min(self.size[0], r + 2)):
+                        for j in range(max(0, c - 1), min(self.size[1], c + 2)):
+                            if self.board_values[i][j] != "*" and self.board_values[i][j] != 0 and self.board_show[i][j] == False:
+                                self.board_show[i][j] = True
+                                self.free_position -= 1
+                            if self.board_values[i][j] == 0 and self.board_show[i][j] == False:
+                                self.board_show[i][j] = True
+                                self.free_position -= 1
+                                clear_location(i,j)
+                                        
+            #If you dig a "cero", then execute clear_location()    
+            if self.board_values[row][col] == 0:
+                self.board_show[row][col] = True
+                self.free_position -= 1
+                clear_location(row, col)
+                self.show_board()    
+            
+            if self.free_position > 0:    
+                return True
+            else:
+                print("")
+                print("Free postion left: ", end="")
+                self.free()
+                global final_time
+                final_time = time.time()
+                print("Congratulation! YOU WIN!")
+                print("Your time: ", final_time - start_time, "seconds.")
+                return False
+        except IndexError:
+            print("Enter a valid coordinate!")    
             return True
-        else:
-            print("")
-            print("Free postion left: ", end="")
-            self.free()
-            global final_time
-            final_time = time.time()
-            print("Congratulation! YOU WIN!")
-            print("Your time: ", final_time - start_time, "seconds.")
-            return False
+            
 
 print("Welcome  to Minesweeper!")
 print("The coordinates must be have the format: row,column (with the ',' and without spaces)")
@@ -154,16 +162,20 @@ game.show_board()
 
 on_game = True
 show = [None, None]
+start_time = time.time()
 
-while on_game:
-    start_time = time.time()
-    print("")
-    print("Free postion left: ", end="")
-    game.free()
-    select = (input("Choose the coordinates: ")).split(",")
-    print("")
-    show[0], show[1] = int(select[0]), int(select[1])
-    on_game = game.look_at(show[0], show[1])
+while on_game:    
+    try:
+        print("")
+        print("Free postion left: ", end="")
+        game.free()
+        select = (input("Choose the coordinates: ")).split(",")
+        print("")
+        show[0], show[1] = int(select[0]), int(select[1])
+        on_game = game.look_at(show[0], show[1])
+    except:
+        print("Enter a valid coordinate!")    
+        continue   
     
     if not on_game:
         print("")
@@ -176,6 +188,7 @@ while on_game:
             if choised == "s":
                 game.reset()
                 game.new_board()
+                start_time = time.time()
                 game.show_board()
                 on_game = True
             elif choised == "n":
