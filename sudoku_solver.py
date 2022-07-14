e13116b3a3f129471de0be38fc129b2701e43245
Leonardo-JK <leok_86@hotmail.com>
2022-07-14 18:44:22 -0300

    Final version

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,15 +1,18 @@
 import math
 import time
 
+from traitlets import ValidateHandler
+
 
 class Board:
         
     def __init__(self,matrix, on_seting=False):
-        self.values = matrix
-        self.valid_numbers = [[[1,2,3,4,5,6,7,8,9] for i in range(9)] for j in range(9)]
-        self.verified_values = [[False for i in range(9)] for j in range(9)]
-        self.on_seting = on_seting
+        self.values = matrix    # Contain the correct values for each box.
+        self.valid_numbers = [[[1,2,3,4,5,6,7,8,9] for i in range(9)] for j in range(9)]  # Contain all posible numbers for each box.
+        self.verified_values = [[False for i in range(9)] for j in range(9)]  # Contain which box has all ready with a correct number.
+        self.on_seting = on_seting # State that prevent run the test if there are numbers setted. 
     
+    # This method print the board on screen.
     def show_board(self):
         def index_x():
             j = 0
@@ -93,82 +96,73 @@ class Board:
                 print("")
                 j += 1
                 
-                
+    # This method set a correct number on a box which was given to it.            
     def set_values(self, v, h, val, send, k=None, l=None):
+        if self.verified_values[v][h] == True:
+            print("This position have a number settled already.")
+            return
         
+        print("")
+        print("Linea: ", v, " - Columna: ", h, " - Numero: ", val, " - Enviado por: ", send, " (", k, ", ", l, ")", end=" ->")
+                
+        self.values[v][h] = val
+        self.verified_values[v][h] = True
+        self.valid_numbers[v][h].clear()
+        self.valid_numbers[v][h].append(val)
         
-            print("")
-            print("Linea: ", v, " - Columna: ", h, " - Numero: ", val, " - Enviado por: ", send, " (", k, ", ", l, ")", end=" ->")
-                    
-            self.values[v][h] = val
-            self.verified_values[v][h] = True
-            self.valid_numbers[v][h].clear()
-            self.valid_numbers[v][h].append(val)
+        # Eliminate the correct number from the list of posible number on all the others box on the same line.
+        for r in range(9):
+            if r == h:
+                continue
             
-            for r in range(9):
-                if r == h:
+            if val not in self.valid_numbers[v][r]:
+                continue
+            else:
+                self.valid_numbers[v][r].remove(val)        
+
+        # Eliminate the correct number from the list of posible number on all the others box on the same column.
+        for c in range(9):
+            if c  == v:
+                continue
+            
+            if val not in self.valid_numbers[c][h]:
+                continue
+            else:
+                self.valid_numbers[c][h].remove(val)
+        
+        # Eliminate the correct number from the list of posible number on all the others box on the same square.
+        hs = math.ceil((h + 1) / 3) - 1 # Auxiliar coordinate to select the rigth square
+        vs = math.ceil((v + 1) / 3) - 1 #
+            
+        for i in range(3):
+            for j in range(3):
+                if (hs * 3) + i == h and (vs * 3) + j == v:
                     continue
                 
-                if val not in self.valid_numbers[v][r]:
+                if val not in self.valid_numbers[(vs * 3) + j][(hs * 3) + i]:
                     continue
                 else:
-                    self.valid_numbers[v][r].remove(val)        
-            
-            for c in range(9):
-                if c  == v:
-                    continue
-                
-                if val not in self.valid_numbers[c][h]:
-                    continue
-                else:
-                    self.valid_numbers[c][h].remove(val)
-            
-            hs = math.ceil((h + 1) / 3) - 1 
-            vs = math.ceil((v + 1) / 3) - 1  
-                
-            for i in range(3):
-                for j in range(3):
-                    if (hs * 3) + i == h and (vs * 3) + j == v:
-                        continue
-                    
-                    if val not in self.valid_numbers[(vs * 3) + j][(hs * 3) + i]:
-                        continue
-                    else:
-                        self.valid_numbers[(vs * 3) + j][(hs * 3) + i].remove(val)
-            
-    
+                    self.valid_numbers[(vs * 3) + j][(hs * 3) + i].remove(val)
+        
+        game.show_board()
+        
+    # This method solve the puzzle.
     def solve(self):
-        changes = False
+        changes = False # State that verify if a modification on the board was made.
         
-        print("Inicio cerificacion UNICO DISPONIBLE:", end="\n\n")
-        #Verifica si solo hay un numero disponible
         for i in range(9):
-            for j in range(9):                
+            not_valid_numb_row = [] # Save repeat numbers to skip when are found again on the list of posible numbers on the same row.
+            not_valid_numb_col = [] # Save repeat numbers to skip when are found again on the list of posible numbers on the same column.
+            for j in range(9):
+                if self.verified_values[i][j] == True:
+                    continue
+
+                # Verify if each box only have one posible number and set it.
                 if len(self.valid_numbers[i][j]) == 1 and self.verified_values[i][j] == False:
                     self.set_values(i, j, self.valid_numbers[i][j][0], "Unico DISPONIBLE", i, j)
                     changes = True
-        print("")            
-        print("Fin verificacion de unico disponible", end="\n\n")
-        for i in range(9):
-                for k in range(9):
-                    print('%20s' % game.valid_numbers[i][k], sep=" - ", end="")
-                print("")
-        print("\n\n")
-        
-        if changes == True:
-            print("REINICIO DE COMPROVACIONES")
-            print("")
-            self.solve()
-            
-        # 
-        print("Inicio verificacion de LINEAS:", end="\n\n")
-        #Verifica si es el unico posible en la linea
-        for i in range(9):
-            not_valid_numb_row = []   
-            for j in range(9):
-                if self.verified_values[i][j] == True:
-                    continue  
-                
+
+                # For each posible number on the list of posible numbers, verify if only can be in that box for each line.
                 for num in self.valid_numbers[i][j]:
                     if num in not_valid_numb_row:
                             continue
@@ -180,36 +174,13 @@ class Board:
                         if num in self.valid_numbers[i][k]:
                             not_valid_numb_row.append(num)
                             break
-                    print("Linea: ", i)    
-                    print(not_valid_numb_row)
-                    print("")
                     
                     if num not in not_valid_numb_row:    
                         self.set_values(i, j, num, "Unico en la LINEA", i)
-                        changes = True       
-        print("")                    
-        print("Fin verificacion de LINEAS.", end="\n\n")
-        for i in range(9):
-            for k in range(9):
-                print('%20s' % game.valid_numbers[i][k], sep=" - ", end="")
-            print("")
-        print("\n\n")
-                    
-        if changes == True:
-            print("REINICIO DE COMPROVACIONES")
-            print("")
-            self.solve()    
-            
-        print("Inicio de verificacion de COLUMNAS:", end="\n\n")               
-        #Verifica si es el unico en la columna  
-        for i in range(9):
-            not_valid_numb_col = []   
-            for j in range(9):
-                if self.verified_values[j][i] == True:
-                    continue
-                
-                for num in self.valid_numbers[j][i]: 
-                    print("Columna: ", i, "Linea: ", j, " - Numero: ", num)     
+                        changes = True
+
+                # For each posible number on the list of posible numbers, verify if only can be in that box for each column.
+                for num in self.valid_numbers[j][i]:     
                     if num in not_valid_numb_col:
                             continue
                     
@@ -221,37 +192,22 @@ class Board:
                             not_valid_numb_col.append(num)
                             break
                     
-                    print("Numeros repetidos: ", not_valid_numb_col)
-                    print("") 
-                    
                     if num not in not_valid_numb_col:
                         self.set_values(j, i, num, "Unico en la COLUMNA", j)
                         changes = True
-        print("")                
-        print("Fin verificacion de COLUMNAS.", end="\n\n")
-        for i in range(9):
-            for k in range(9):
-                print('%20s' % game.valid_numbers[i][k], sep=" - ", end="")
-            print("")
-        print("\n\n")
-                    
-        if changes == True:
-            print("REINICIO DE COMPROVACIONES")
-            print("")
-            self.solve()
-        
-        print("Inicio de verificacion de CUADRADOS:", end="\n\n")
-        #Verifica si es el unico en el cuadrado
+
+        # Iterate for each on of the nine squares.
         for i in range(3):
             for j in range(3):
-                not_valid_numb_sqr =[] 
+                not_valid_numb_sqr =[] # Save repeat numbers to skip when are found again on the list of posible numbers on the same square.
                 for k in range(9):
-                    I = (math.ceil((k + 1) / 3) - 1) + i*3
+                    I = (math.ceil((k + 1) / 3) - 1) + i*3  # Transform the inner coordinate to real coordinate.
                     J = (math.ceil((k + 1)) - 1) % 3 + j*3
                     
                     if self.verified_values[I][J] == True:
                         continue
                     
+                    # For each posible number on the list of posible numbers, verify if only can be in that box for each square.
                     for num in self.valid_numbers[I][J]:
                         if num in not_valid_numb_sqr:
                             break
@@ -266,48 +222,37 @@ class Board:
                             if num in self.valid_numbers[y][x]:
                                 not_valid_numb_sqr.append(num)
                                 break
-                        print("Cuandrado: ", i, j)
-                        print(not_valid_numb_sqr)
-                        print("") 
-                    
+                        
                         if num not in not_valid_numb_sqr:
                             self.set_values(I, J, num, "Unico en el CUADRADO", i, j)
                             changes = True
-        print("")                
-        print("Fin verificacion CUADRADOS", end="\n\n")
-        for i in range(9):
-            for k in range(9):
-                print('%20s' % game.valid_numbers[i][k], sep=" - ", end="")
-            print("")
-        print("\n\n")                    
-                    
+
+        # If the board has a change then ejecute the verification again. 
+        # Other wise verify if all boxes were solved and determinate if the puzzle was solved or not          
         if changes == True:
             self.solve()
         else:
-            sum = 0
-            
             for i in range(9):
                 for j in range(9):
-                    sum += len(self.valid_numbers[i][j])  
-            
-            if sum == 81:
-                print("PUZZLE SOLVED!")
-                self.show_board()
-                exit()
-            else:              
-                print("It's unsolveble")
-                self.show_board()
-                exit()                        
+                    if self.verified_values[i][j] == False:
+                        print("It's unsolveble")
+                        self.show_board()
+                        exit()
+
+            print("PUZZLE SOLVED!")
+            self.show_board()
+            return
+                            
         
                     
-test_matrix = [['-','-','-','-','-','-','-','-','-'],['-',2,'-','-',5,'-',6,'-','-'],['-',8,'-','-',4,'-',1,'-','-'],['-','-','-','-',2,9,'-','-',1],['-','-',1,8,'-','-','-',7,'-'],['-',5,'-',3,'-','-','-',8,'-'],['-',3,'-','-','-',8,'-','-',7],['-','-','-',1,'-','-',5,6,'-'],[7,'-','-','-','-',6,'-','-',9]]
+test_matrix = [['-','-','-','-','-','-','-',6,3],[2,3,'-','-','-','-',1,7,'-'],[1,'-',4,'-','-','-','-','-','-'],['-',2,'-','-','-','-',4,'-','-'],['-','-',6,8,5,3,'-','-','-'],['-','-','-','-',7,'-',9,'-','-'],['-','-','-',5,9,6,'-','-',8],['-',4,'-','-','-','-',3,'-','-'],['-',7,'-','-',8,'-','-','-','-']]
 matrix = [["-" for i in range(9)] for j in range(9)]
 game = Board(matrix)
 
 game.show_board()
 
 solved = False
-print("Introduce the coordinates and the value in this format: 'v h #'")
+print("Introduce the coordinates and the value in this format: 'vh#'.")
 
 def test(m):
     print(game.valid_numbers)
@@ -330,19 +275,25 @@ def test(m):
                 
 while not solved:
     data = (input("Enter new value or 'solve': "))
-    print(data)
+            
+
+            
     if data == "solve":
+        start = time.time()
         game.solve()
-        game.show_board()
+        print(time.time() - start, " segundos.")
         solved = True
     elif data == "test":
         test(test_matrix)
         game.show_board()
     else:
-        values = data.split(" ")
-        print(values)
-        print(game.valid_numbers)
-        game.set_values(int(values[0]), int(values[1]), int(values[2]), "User")
-        game.show_board()
-        print(game.valid_numbers)
-        
+        try:
+            print(data)
+                        
+            if len(data) != 3 or int(data[0]) < 0 or int(data[0]) > 8 or int(data[1]) < 0 or int(data[1]) > 8 or int(data[2]) < 1 or int(data[2]) > 9:
+                raise ValueError
+            
+            game.set_values(int(data[0]), int(data[1]), int(data[2]), "User")
+        except ValueError:
+            print("Please, enter a correct input.")     
+            
